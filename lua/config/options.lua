b57c70e598661c8b5d9537e87919a4ae2786153a
herschel-ma <maliang_gs@126.com>
2023-12-21 21:19:13 +0800

    feat: change terminal to nu(installed by scoop)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,19 +3,24 @@
 -- Add any additional options here
 -- vim.o.background = "light"
 
--- set default shell to pwsh in windows.
+-- set default shell to nushell|pwsh in windows.
+-- https://github.com/neovim/neovim/issues/19648
 local powershell_options = {
-  shell = vim.fn.executable("pwsh") and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shell = vim.fn.executable("nu") and "nu.exe",
+  shellcmdflag = "-c",
   shellquote = "",
   shellxquote = "",
+
+  -- shell = vim.fn.executable("pwsh") and "pwsh" or "powershell",
+  -- shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  -- shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  -- shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  -- shellquote = "",
+  -- shellxquote = "",
 }
 
-
 if vim.fn.has("win32") then
-  for option, value in pairs(powershell_options) do 
+  for option, value in pairs(powershell_options) do
     vim.o[option] = value
   end
 end
