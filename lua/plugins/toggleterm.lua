6247154acf43fb917d76c6e0019c99bd11bdbf0f
herschel-ma <maliang_gs@126.com>
2023-12-21 22:04:18 +0800

    feat: change terminal to float

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,9 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "float",
+    },
+  },
 }
