b4af511c28ef7aa849b2021061098fb1bda382ae
herschel-ma <maliang_gs@126.com>
2024-01-10 16:21:51 +0800

    feat(noice): add border to lsp doc/signature popup window. loc: lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "SmiteshP/nvim-navbuddy",
-      dependencies = {
-        "SmiteshP/nvim-navic",
-        "MunifTanjim/nui.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "SmiteshP/nvim-navbuddy",
+        dependencies = {
+          "SmiteshP/nvim-navic",
+          "MunifTanjim/nui.nvim",
+        },
+        opts = { lsp = { auto_attach = true } },
       },
-      opts = { lsp = { auto_attach = true } },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      health = {
+        checker = true,
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
     },
   },
 }
