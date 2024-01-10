069f9ab02f745026f397fcaa38757a9a39508b43
herschel-ma <maliang_gs@126.com>
2024-01-10 14:53:59 +0800

    feat(theme): enhance catputtin theme experience

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   {
     "craftzdog/solarized-osaka.nvim",
-    lazy = false,
+    lazy = true,
     priority = 1000,
     opts = {
       transparent = true,
@@ -15,6 +15,7 @@ return {
   -- optional 2 (high contrast, light one is nice)
   {
     "miikanissi/modus-themes.nvim",
+    lazy = true,
     priority = 1000,
     opts = {
       style = "modus_operandi",
@@ -22,7 +23,50 @@ return {
     },
   },
   -- optional 3 (it's pretty)
-  { "akinsho/horizon.nvim", version = "*" },
+  { "akinsho/horizon.nvim", lazy = true, priority = 1000, version = "*" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      term_colors = true,
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = {}, -- Change the style of comments
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = { "bold" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+    },
+    integrations = {
+      dap = true,
+      dap_ui = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+        inlay_hints = { background = true },
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
