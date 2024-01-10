8a9f8a528ad1450ddd9dd1771dd202d70da07450
herschel-ma <maliang_gs@126.com>
2024-01-10 15:44:22 +0800

    feat(cmp): just set cmp's item.menu to "", because it is too long!

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,13 +1,23 @@
+local cmp = require("cmp")
+
 return {
   "hrsh7th/nvim-cmp",
   -- additional config
-  opts = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-    })
-  end,
+  opts = {
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+
+    formatting = {
+      format = function(_, item)
+        local icons = require("lazyvim.config").icons.kinds
+        item.menu = ""
+        if icons[item.kind] then
+          item.kind = icons[item.kind] .. item.kind
+        end
+        return item
+      end,
+    },
+  },
 }
